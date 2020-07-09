8bc890514c273e81f8fba8beeb4a03045be2fc00
skywalker6666 <50949184+skywalker6666@users.noreply.github.com>
2020-07-09 23:55:45 +0800

    successfully_connected_to_firebase
    ya

--- src/component/toolbar/toolbar-item-export.js
+++ src/component/toolbar/toolbar-item-export.js
@@ -12,13 +12,14 @@ export function ToolbarItemExport(props) {
 
     const json = controller.run("serializeModel", diagramProps);
     const jsonStr = JSON.stringify(json);
+    const jsonExport = JSON.parse(jsonStr);
     const url = `data:text/plain,${encodeURIComponent(jsonStr)}`;
     const title = controller.run("getTopicTitle", {
       ...diagramProps,
       topicKey: model.rootTopicKey
     });
     downloadFile(url, `${title}.blinkmind`)
-    exportToFirebase(jsonStr);
+    exportToFirebase(jsonExport);
     
   };
 
